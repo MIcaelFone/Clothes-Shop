803c2cbc0df7f9c1cbde758886cad5ab27f24060
Micael Fone <micaelfone0@gmail.com>
2024-04-06 11:30:37 -0300

    Adicionando descrição sobre as marcas serem trabalhadas

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,10 +8,7 @@ function Home() {
   return (
     
     <div>
-      
-      <br></br>
-
-      <div style={{border:'2px solid black'}}>
+      <div style={{border:'1px solid black'}}>
           <Container fluid>
             <Row>
               <Col style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
@@ -28,27 +25,39 @@ function Home() {
             </Row>
           </Container>
       </div>
-          
-      <br></br>
-      
+    <br></br>
       <section class="banner-sections">
-        <Container>
-          <Col>
-            <Image src="https://i0.wp.com/www.canalmasculino.com.br/wp-content/uploads/2019/05/moda-masculina-destaque-usa-se-quiser.jpg?resize=564%2C705" style={{height:'50vh', borderRadius:'100%', margin:'10px'}} roundedCircle />
-            <h3>Moda Masculina</h3>
-          </Col>
-          <Col>
-            <Image src="https://i0.wp.com/www.canalmasculino.com.br/wp-content/uploads/2019/05/moda-masculina-destaque-usa-se-quiser.jpg?resize=564%2C705" style={{height:'50vh', borderRadius:'100%', margin:'10px'}} roundedCircle />
-            <h3>Moda Feminina</h3>
-          </Col>
-          <Col>
-            <Image src="https://i0.wp.com/www.canalmasculino.com.br/wp-content/uploads/2019/05/moda-masculina-destaque-usa-se-quiser.jpg?resize=564%2C705" style={{height:'50vh', borderRadius:'100%', margin:'10px'}} roundedCircle />
-            <h3>Moda Infantil</h3>
-          </Col>
-        </Container>
+          <Container>
+            <Col style={{ display: 'flex', justifyContent: 'center' ,gap:'3rem' }}>
+                <div style={{ marginRight: '10px' }}>
+                  <Image src="https://i0.wp.com/www.canalmasculino.com.br/wp-content/uploads/2019/05/moda-masculina-destaque-usa-se-quiser.jpg?resize=564%2C705" style={{ height: '50vh', borderRadius: '100%' }} roundedCircle />
+                 <center> <h4>Moda Masculina</h4></center>
+                </div>
+                <div style={{ marginRight: '10px' }}>
+                  <Image src="https://images.tcdn.com.br/img/img_prod/919154/short_saia_2037_1_06f0bb5affc8f4d67b2ca7573f4bdd4a.jpg" style={{ height: '50vh', borderRadius: '100%' }} roundedCircle />
+                  <center><h4>Moda Feminina</h4></center>
+                </div>
+                <div>
+                  <Image src="https://img.elo7.com.br/product/main/3DE8EE4/vestido-longo-tricoline-bolinha-poa-azul-moda-infantil-2021.jpg" style={{ height: '50vh', borderRadius: '100%' }} roundedCircle />
+                  <center> <h4>Moda Infantil</h4> </center>
+                </div>
+            </Col>
+          </Container>
       </section>
+
       <br></br>
-   
+     <h1 style={{textAlign: 'center'}}>AS MELHORES MARCAS</h1>
+      <div>
+          <Container>
+            <Row>
+              <Col style={{ display: 'flex', justifyContent: 'center' }}>
+                <Image src="https://i0.wp.com/www.canalmasculino.com.br/wp-content/uploads/2019/05/moda-masculina-destaque-usa-se-quiser.jpg?resize=564%2C705" style={{ height: '50vh', borderRadius: '100%' }}/>
+                <Image src="https://i0.wp.com/www.canalmasculino.com.br/wp-content/uploads/2019/05/moda-masculina-destaque-usa-se-quiser.jpg?resize=564%2C705" style={{ height: '50vh', borderRadius: '100%' }} roundedCircle />
+                <Image src="https://i0.wp.com/www.canalmasculino.com.br/wp-content/uploads/2019/05/moda-masculina-destaque-usa-se-quiser.jpg?resize=564%2C705" style={{ height: '50vh', borderRadius: '100%' }} roundedCircle />
+              </Col>
+            </Row>
+          </Container>
+      </div>
     </div>
   );  
 }
