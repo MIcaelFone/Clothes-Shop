96319eb2eb42bd1fbcf34a04ab74862353f4a6a7
Micael Fone <104154840+MIcaelFone@users.noreply.github.com>
2024-04-13 14:40:31 -0300

    Pagina de ajuste e suporte (#8)
    * Desenvolvendo paginas de ajuste e suporte  no footer
    
    * Ajustando as rotas  de paginas

---
+++ src/pages/como_comprar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function como_comprar(){
+    return(
+
+        <div>
+            <br></br>
+            <center><h3>Como Comprar seu Clothes Shop</h3> </center>
+            
+                <ol>
+                    <li>Basta navegar entre as páginas de compra e clicar sobre o ícone Comprar. Ou clique em Ver Detalhes para maiores informações sobre o produto. Após isso o produto será inserido em seu carrinho de compras</li>
+                    <li>Continue navegando em nosso site até escolher todos os produtos que você deseja adquirir. Agora basta clicar no link a direita Finalizar que você estará concluindo sua compra;</li>
+                    <li>Agora entre com o seu email e senha. Caso não for cadastrado, cadastre-se rapidamente clicando no link de Cadastro</li>
+                    <li>Feito isso, escolha a forma de envio do produto. Trabalhamos com envio via Correio  </li>
+                    <li>Agora basta escolher a forma de pagamento;</li>
+                </ol>
+            
+        </div>
+    )
+
+}
+export default como_comprar;
