7ee1e4fb28bd6b1532b1eb900d7ed972769fbb06
Micael Fone <micaelfone0@gmail.com>
2024-05-03 00:31:17 -0300

    Cadstrando produto

--- frontend/src/ui/Components/moda/cadastroproduto.js
+++ frontend/src/ui/Components/moda/cadastroproduto.js
@@ -5,7 +5,7 @@ import "../../styles/Tela_cadastro_produto.css"
 import imagemRoupas from '../../../assests/roupas-masculinas-estilosas-com-jeans-e-camiseta.webp';
 import { toast } from "react-toastify";
 import axios from 'axios';
-import { response } from 'express';
+
 
 const isNomeValid=(nome)=>{
     const pattern= new RegExp("[A-Za-z]+");
@@ -96,21 +96,32 @@ function Tela_cadastro_produto() {
     return (
         <>
             <div style={{backgroundImage: `url(${imagemRoupas})`}}>
+                
                 <div className="Cadastro template d-flex justify-content-center align-items-center  bg-white">
+                    
                     <div className="form_container p-3 rounded bg-white">
+                            
                             <center><h2>Cadastro de produto</h2> </center>
+                            
                             <form onSubmit={eventsubmit}>
+                                
                                 <label htmlFor='nome_produto'>Nome do produto</label>
+                                
                                 <Form.Control type="text" name='nome' placeholder="Nome do produto" id='nome_produto' required  value={nome} onChange={handleChangeverificationname}  />                    
+                                
                                 <label htmlFor='marca'>Marca</label>
+                                
                                 <Form.Control type="text" id="marca" name='marca' placeholder="Marca" required  value={marca} onChange={handleChangeverificationmarca}/>
+                                
                                 <label htmlFor='marca'>Preço</label>
+                                
                                 <InputGroup className="mb-2">
                                     <InputGroup.Text placeholder='Preço do produto'  value={preco} onChange={handleChangepreco} >$</InputGroup.Text>
                                     <Form.Control aria-label="Amount (to the nearest dollar)" name='preco' placeholder='123.50' value={preco} onChange={handleChangepreco} required />                          
                                 </InputGroup>
                                                                        
                                 <label htmlFor='moda'>Tipo de moda</label>
+                                
                                 <Form.Select value={moda} onChange={(e)=>setModa(e.target.value)}>
                                     <option value={"moda_masculina"}>Moda masculina</option>
                                     <option value={"moda_feminina"} >Moda feminina</option>
@@ -120,10 +131,12 @@ function Tela_cadastro_produto() {
                                     <Form.Label>Insira multiplas imagem do produto</Form.Label>
                                     <Form.Control type="file" value={imagem} onChange={handleChangeimagem} required />
                                 </Form.Group>
+
                                 <Form.Group className="mb-2" controlId="exampleForm.ControlTextarea1" >
                                     <Form.Label>Descrição do produto</Form.Label>
                                     <Form.Control as="textarea" rows={3} required  value={descricao} onChange={handleChangeverificationdescricao} />
                                 </Form.Group>
+
                                 <div className='d-grid'>
                                     <button type="submit" className='btn btn-primary' >Cadastrar produto</button> {/* Correção: substituído Link por button e adicionado type="submit" */}
                                 </div>
