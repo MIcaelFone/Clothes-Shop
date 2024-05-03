04b57403bddf952657820ab19196dc4289cc7a1d
Micael Fone <micaelfone0@gmail.com>
2024-05-03 19:06:32 -0300

    Estruturando a rota de post

--- frontend/src/ui/Components/moda/cadastroproduto.js
+++ frontend/src/ui/Components/moda/cadastroproduto.js
@@ -75,15 +75,7 @@ function Tela_cadastro_produto() {
     const eventsubmit = async (event) => {    
         event.preventDefault()
         try{
-            await axios.post("http://localhost:8080/produto/cadastrarproduto",{
-                nome,
-                marca,
-                descricao,
-                preco,
-                imagem,
-                moda, 
-            
-            })
+            await axios.post("http://localhost:8080/produto/cadastrarproduto",{nome,marca,descricao,preco,imagem,moda})
             .then((response) => {
                 console.log(response.data)
             })
