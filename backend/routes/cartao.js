caf0c43977a011a5700f294b8864df1f3150bc6e
Micael Fone <micaelfone0@gmail.com>
2024-06-11 19:47:55 -0300

    Rotas para chamar api cartao

--- backend/routes/cartao.js
+++ backend/routes/cartao.js
@@ -1,6 +1,10 @@
-const {cadastrandocartao} = require("../controllers/cartao")
+const {cadastrandocartao,buscarCartaousuario,buscarcartaonumeroCartao,atualizaCartao,deleteCartao} = require("../controllers/cartao")
 const express = require('express');
 const router = express.Router();
 
 router.post("/cadastrarcartao",cadastrandocartao)
+router.post("/buscarcartao",buscarCartaousuario)
+router.post("/buscarcartaonumerocartao",buscarcartaonumeroCartao)
+router.put("/atualizacartao",atualizaCartao)   
+router.delete("/deletacartao",deleteCartao)
 module.exports=router
