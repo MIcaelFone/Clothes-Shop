fd803027cb65c2633265e2a7d5cca2b27171954f
Micael Fone <micaelfone0@gmail.com>
2024-06-10 22:49:29 -0300

    Tirando rota de teste

--- backend/routes/produto.js
+++ backend/routes/produto.js
@@ -1,8 +1,7 @@
-const {addProduto, deleteProduto, updateProduto, getProduto, test,getProdutofeminino,getProdutomasculino, buscandoprodutoespecifico} = require('../controllers/produto');
+const {addProduto, deleteProduto, updateProduto, getProduto,getProdutofeminino,getProdutomasculino, buscandoprodutoespecifico} = require('../controllers/produto');
 const express = require('express');
 const router = express.Router();
 
-router.get("/helloworld", test);
 router.get("/produtosmasculino",  getProdutomasculino);
 router.get("/produtosfeminino", getProdutofeminino);
 router.get('/cadastroprodutolistar',  getProduto);
