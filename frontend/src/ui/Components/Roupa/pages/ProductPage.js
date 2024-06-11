71f6b247965c5660cbfcfcdca6dbf09140a1b530
Micael Fone <micaelfone0@gmail.com>
2024-06-10 23:15:12 -0300

    Trazendo os dados individuais do produto

--- frontend/src/ui/Components/Roupa/pages/ProductPage.js
+++ frontend/src/ui/Components/Roupa/pages/ProductPage.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState, useContext } from "react";
 import { Button } from "react-bootstrap";
 import axios from "axios";
 import { toast } from 'react-toastify';
 import { FormattedMessage } from "react-intl";
 import { FaShoppingCart } from "react-icons/fa";
-const ProductPage = ({ cart }) => {
+import { CartContext } from '../../Carrinhoprodutos/config/Cartprovider'; // Importe o contexto do carrinho
+import '../styles/produto.css'; // Importe o arquivo CSS criado
+
+const ProductPage = () => {
   const [nomeProduto, setNomeProduto] = useState('');
   const [produto, setProduto] = useState(null);
-  const [amount, setAmount] = useState(1);
-  const dispatch = useDispatch();
-
+  const { cartItems, addToCart, removeFromCart } = useContext(CartContext); // Use o contexto do carrinho
   useEffect(() => {
     const url = decodeURIComponent(window.location.href);
     const tamanho = url.split("/");
@@ -18,14 +18,13 @@ const ProductPage = ({ cart }) => {
     console.log(nomeProdutoFromUrl);
     setNomeProduto(nomeProdutoFromUrl);
   }, []);
-
   useEffect(() => {
     if (nomeProduto) {
       const fetchProduto = async () => {
         try {
-          const response = await axios.post("http://localhost:8080/produto/buscandoprodutoespecifico", {nome: nomeProduto });
+          const response = await axios.post("http://localhost:8080/produto/buscandoprodutoespecifico", { nome: nomeProduto });
           if (response.status === 200) {
-            setProduto(response.data.data[0]);  // Assuming the backend returns an array of products
+            setProduto(response.data.data[0]);  // Assumindo que o backend retorna um array de produtos
             console.log("Produto voltou para o frontend");
             console.log(response.data.data);
           }
@@ -37,49 +36,87 @@ const ProductPage = ({ cart }) => {
     }
   }, [nomeProduto]);
 
-  const handleAddToCart = () => {
-    if (produto) {
-      console.log("Adicionando ao carrinho:", produto);
-      dispatch({ type: "ADD_TO_CART", payload: { ...produto, qty: amount } });
+  const notifyAddedToCart = (item) => toast.success( <> {item.nome} <FormattedMessage id='adicao_produto' /> </>, {
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    theme: 'colored',
+    style: {
+      backgroundColor: '#fff',
+      color: '#000',
     }
-  };
+  });
 
-  const handleRemoveFromCart = () => {
-    if (produto) {
-      console.log("Removendo do carrinho:", produto);
-      dispatch({ type: "REMOVE_FROM_CART", payload: produto });
+  const notifyRemovedFromCart = (item) => toast.error(<>{item.nome} <FormattedMessage id='removendo_produto'></FormattedMessage> </>, {
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    theme: 'colored',
+    style: {
+      backgroundColor: '#000',
+      color: '#fff',
     }
+  });
+
+  const handleRemoveFromCart = (product) => {
+    removeFromCart(product);
+    notifyRemovedFromCart(product);
   };
 
   return (
-    <div className="d-flex flex-column flex-lg-row gap-4 mt-2 " style={{marginLeft:"2rem"}}>
+    <div className="product-container">
       {produto ? (
-        <div className="d-flex flex-column">
+        <div className="product-details">
           <div>
-            <span className="text-primary fw-semibold">{produto.marca}</span>
-            <h1 className="fs-1 fw-bold">{produto.nome}</h1>
+            <span className="product-brand">{produto.marca}</span>
+            <h1 className="product-name">{produto.nome}</h1>
           </div>
-          <p className="text-muted">{produto.descricao}</p>
+          <p className="product-description">{produto.descricao}</p>
           <div>
-            <h6 className="fs-4 fw-semibold"><FormattedMessage id="money">R$</FormattedMessage> {produto.preco}</h6>
+            <h6 className="product-price"><FormattedMessage id="money" /> {produto.preco}</h6>
           </div>
-          <div className="d-flex flex-row align-items-center gap-3">
-            <div className="d-flex flex-row align-items-center ">
-              <button
-                className="bg-light px-4 text-dark fs-3"style={{height:"3rem", marginTop:"0.5rem"}} 
-                onClick={() => setAmount((prev) => Math.max(prev - 1, 1))}
-              >
-                -
-              </button>
-              <span className="py-2 px-3 rounded">{amount}</span>
-              <button
-                className="bg-light px-4 text-dark fs-3" style={{height:"3rem", marginTop:"0.5rem"}}
-                onClick={() => setAmount((prev) => prev + 1)}
-              >
-                +
-              </button>
-            </div>
-            <FaShoppingCart size={35} style={{ marginLeft: '10px', color: 'black' }} />
+          <div className="quantity-container">
+            {cartItems.find(item => item.idproduto === produto.idproduto) ? (
+              <React.Fragment>
+                <button 
+                  className="button"
+                  onClick={() => addToCart(produto)}
+                >
+                  +
+                </button>
+                <p className="quantity-text">
+                  {cartItems.find(item => item.idproduto === produto.idproduto)?.quantity}
+                </p>
+                <button
+                  className="button"
+                  onClick={() => {
+                    const cartItem = cartItems.find((item) => item.idproduto === produto.idproduto);
+                    if (cartItem.quantity === 1) {
+                      handleRemoveFromCart(produto);
+                    } else {
+                      removeFromCart(produto);
+                    }
+                  }}
+                >
+                  -
+                </button>
+              </React.Fragment>
+            ) : (
+              <FaShoppingCart
+                size={35}
+                style={{ marginLeft: '10px', color: 'black', cursor: 'pointer' }}
+                onClick={() => {
+                  addToCart(produto);
+                  notifyAddedToCart(produto);
+                }}
+              />
+            )}
           </div>
         </div>
       ) : (
