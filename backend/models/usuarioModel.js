bb1ae2e712e4dee152b1dac0e8fd85141e34645a
Micael Fone <micaelfone0@gmail.com>
2024-07-15 06:30:31 -0300

    Não faço ideia disso kkkkkk

--- backend/models/usuarioModel.js
+++ backend/models/usuarioModel.js
@@ -1,32 +1,33 @@
 const { DataTypes } = require('sequelize');
 const db = require('../Database/database');
-const Usuario=db.define("Usuario",{
-    idusuario:{
-        type:DataTypes.INTEGER,
-        allowNull:false,
-        autoIncrement:true,
-        primaryKey:true
+const Usuario = db.define("Usuario", {
+    idusuario: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
     },
-    nome:{
-        type:DataTypes.STRING,
-        allowNull:false,
-        unique:true
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
     },
-    email:{
-        type:DataTypes.STRING,
-        allowNull:false,
-        unique:true
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
     },
-    senha:{
-        type:DataTypes.STRING,
-        allowNull:false
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    telefone:{
-        type:DataTypes.STRING,
-        allowNull:false
+    telefone: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
-},{
+}, {
     tableName: 'usuario',
     timestamps: false
 });
- module.exports = Usuario;
+
+module.exports = Usuario;
