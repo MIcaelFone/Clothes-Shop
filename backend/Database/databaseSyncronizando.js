1897954300fe6a2b80c43dc09886d72326a2d428
Micael Fone <micaelfone0@gmail.com>
2024-07-15 06:20:51 -0300

    Melhorando a sincronia das tabelas

--- backend/Database/databaseSyncronizando.js
+++ backend/Database/databaseSyncronizando.js
@@ -1,31 +1,27 @@
-const db = require('./database');
-const User = require('../models/usuarioModel'); // Importe o modelo criado
-const Admin = require('../models/AdministradorModel'); // Importe o modelo criado
-const Produto = require('../models/produtoModel'); // Importe o modelo criado
-const Pedidoitem = require('../models/PedidoItemModel');
-const Cartao = require('../models/CartaoModel');
-const Compra = require('../models/CompraModel');  
+const db = require('../Database/database');
+const Usuario = require('../models/usuarioModel');
+const Cartao = require('../models/cartaoModel');
+const Compra = require('../models/PedidoCompraModel');
+const PedidoItem = require('../models/PedidoItemModel');
+const Produto = require('../models/produtoModel');
+const associates = require('../Database/associates');
+
+// Relacionamentos entre os modelos
+ 
 
 const syncDatabase = async () => {
     try {
-        await db.sync({ force: false }); // `force: true` recria as tabelas a cada reinicialização do servidor, remova para não perder dados existentes
-        console.log('Banco de dados sincronizado com sucesso');
-        await Cartao.sync({ force: false })
-        console.log('Tabela cartão sincronizada com o banco de dados');
-        await Compra.sync({ force: false })
-        console.log('Tabela compra sincronizada com o banco de dados');
-        await User.sync({ force: false });
-        console.log('Tabela usuário sincronizada com o banco de dados');
-        await Admin.sync({ force: false });
-        console.log('Tabela administrador sincronizada com o banco de dados');
-        await Produto.sync({ force: false });
-        console.log('Tabela produto sincronizada com o banco de dados');
-        await Pedidoitem.sync({ force: false });
-        console.log('Tabela pedido item sincronizada com o banco de dados');
-        // Adicione lógica adicional aqui se necessário, por exemplo, a criação de registros iniciais
-    } catch (err) {
-        console.error('Erro ao sincronizar o banco de dados:', err);
+        await db.authenticate();
+        console.log('Conexão com o banco de dados estabelecida com sucesso.');
+        await Usuario.sync({});
+        await Cartao.sync({});
+        await Produto.sync({});
+        await Compra.sync({});
+        await PedidoItem.sync({});
+        console.log('Banco de dados sicronizados!');
+        associates();
+    } catch (error) {
+        console.error('Erro ao sincronizar o banco de dados:', error);
     }
 };
-
-module.exports = syncDatabase;
+module.exports=syncDatabase;
