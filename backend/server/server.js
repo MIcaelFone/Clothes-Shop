3a8a25564f66632d3ad4649d1e591e56bd4fd5ef
Micael Fone <micaelfone0@gmail.com>
2024-07-15 06:26:22 -0300

    Criando grafico a partir do servidor

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,20 +1,25 @@
-const produtoRoutes = require("../routes/ProdutoRoutes");
+const produtoRoutes = require("../routes/produtoRoutes");
 const usuarioRoutes = require("../routes/usuarioRoutes");
 const cartaoRoutes = require("../routes/cartaoRoutes");
-const pedidoRoutes = require("../routes/compraRoutes");
+const pedidoRoutes = require("../routes/pedidoCompraRoutes");
 const adminRoutes = require("../routes/AdministradorRoutes");
 const express = require("express");
 const bodyParser = require('body-parser'); 
 const cors = require('cors');
 const dbSync = require("../Database/databaseSyncronizando"); 
 const app = express();
+const path = require('path');
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-// Rotas
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
 app.use('/produto', produtoRoutes);
 app.use('/usuario', usuarioRoutes);
 app.use('/cartao', cartaoRoutes);
