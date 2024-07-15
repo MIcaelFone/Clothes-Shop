b2d218d56d6c3c32efec9a54da05368fdcb84e09
Micael Fone <micaelfone0@gmail.com>
2024-07-15 06:35:31 -0300

    Mudando o nome da API de cadastrar o pedido de compra

--- backend/routes/pedidoCompraRoutes.js
+++ backend/routes/pedidoCompraRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {addPedido,addItem,retornarInformacao,retornarProdutos}=require('../controllers/compraController')
-router.post("/cadastrarpedido",addPedido)
+const {addPedido,addItem,retornarInformacao,retornarProdutos}=require('../controllers/pedidoCompraController')
+router.post("/adicionarpedido",addPedido)
 router.post("/adicionaritem",addItem)
 router.post("/pedidosrealizados",retornarInformacao)
 router.post("/produtospedidos",retornarProdutos)
